afc656c253d67cf3955ff2507ffad76b12c33e55
Djonatas Borges <interdjonatas@gmail.com>
2024-04-10 08:53:48 -0300

    utilizando autopep8

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from controllers.transaction_controller import router as transaction_router
+from database import create_database
+
+from controllers.api_information import router as api_router
 from controllers.user_controller import router as user_router
 from controllers.expense_controller import router as expense_router
 from controllers.revenue_controller import router as revenue_router
 from controllers.investment_controller import router as investment_router
-from controllers.api_information import router as api_router
-from database import create_database
+from controllers.transaction_controller import router as transaction_router
 
 app = FastAPI()
 
