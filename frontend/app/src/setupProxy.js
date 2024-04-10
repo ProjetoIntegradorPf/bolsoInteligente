ecc98a04bf99948fd939fdce4f5944942dbba171
Djonatas Borges <interdjonatas@gmail.com>
2024-04-10 13:12:33 -0300

    endpoint

--- frontend/app/src/setupProxy.js
+++ frontend/app/src/setupProxy.js
@@ -1,11 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-	app.use(
-		'/api',
-		createProxyMiddleware({
-			target: 'https://bolsointeligente-api.onrender.com',
-			changeOrigin: true
-		})
-	);
+module.exports = function(app) {
+    if (!app) {
+        throw new Error('app is required');
+    }
+
+    const proxyOptions = {
+        target: 'https://bolsointeligente-api.onrender.com',
+        changeOrigin: true
+    };
+
+    app.use('/api', createProxyMiddleware(proxyOptions));
 };
